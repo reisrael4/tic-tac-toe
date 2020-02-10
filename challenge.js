70c4fe56cb2c1435d745713f7473b475a1f6c013
RachelIsrael <racheleisrael4@gmail.com>
2020-02-10 12:14:36 -0500

    button centered

--- challenge.js
+++ challenge.js
@@ -1,15 +1,22 @@
 let square = document.querySelectorAll('.square');
-let button = document.querySelector('.clear')
+let button = document.querySelector('.clear');
 let background = 'red';
+let counter = document.querySelector('.turnCounter')
+counter.innerText = "Red's Turn"
+counter.style.color = 'red';
 for(let i=0; i<square.length; i++){
 square[i].addEventListener('click', colorChange);
 function colorChange(e){
     e.preventDefault();
     square[i].style.backgroundColor = background;
     if(background == 'red'){
-        background = 'blue'
+        background = 'blue';
+        counter.innerText = "Blue's Turn";
+        counter.style.color = 'blue';
     }else{
-        background = 'red'
+        background = 'red';
+        counter.innerText = "Red's Turn";
+        counter.style.color = 'red';
     }
     square[i].removeEventListener('click', colorChange)
     }
