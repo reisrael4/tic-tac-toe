d7b6a0658d13a728bf9c961268b99ce87e44e8a5
RachelIsrael <racheleisrael4@gmail.com>
2020-02-10 15:50:38 -0500

    comparison works

--- challenge.js
+++ challenge.js
@@ -5,14 +5,14 @@ let counter = document.querySelector('.turnCounter');
 counter.innerText = "Red's Turn";
 counter.style.color = 'red';
 let win = [
-    [1, 2, 3], 
-    [4, 5, 6], 
-    [7, 8, 9], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [3, 6, 9], 
-    [1, 5, 9], 
-    [3, 5, 7]
+    ['1', '2', '3'], 
+    ['4', '5', '6'], 
+    ['7', '8', '9'], 
+    ['1', '4', '7'], 
+    ['2', '5', '8'], 
+    ['3', '6', '9'], 
+    ['1', '5', '9'], 
+    ['3', '5', '7']
 ];
 let red = [];
 let blue = [];
@@ -25,24 +25,37 @@ for(let i=0; i<square.length; i++){
             background = 'blue';
             counter.innerText = "Blue's Turn";
             counter.style.color = 'blue';
-            red.push(Number(e.target.id))
+            red.push(e.target.id)
             console.log(red)
+            colorName = 'Red';
+            checkForWinner(red);
         }else{
             background = 'red';
             counter.innerText = "Red's Turn";
             counter.style.color = 'red';
-            blue.push(Number(e.target.id))
+            blue.push(e.target.id)
             console.log(blue)
+            colorName = 'Blue';
+            checkForWinner(blue);
         }
         // console.log(e.target.id)
         square[i].removeEventListener('click', colorChange)
-        checkForWinner();
+        
         }
-    function checkForWinner(){
-        for(let i=0; i<win.length; i++){
-          
-        }
+    function checkForWinner(color){
+            for(let i=0; i<win.length; i++){
+                let count = 0;
+                for(let j=0; j<color.length; j++){
+                    if(color.includes(win[i][j]) === true){
+                        count = count + 1;
+                        if(count === 3){
+                            console.log(colorName + ' wins!' )
+                        }
+                    }
+                }
+         }
     }
+    // if(red.length>=3 || blue.length>= 3){}
     button.addEventListener('click', clearBoard);
     function clearBoard(e){
         e.preventDefault();
