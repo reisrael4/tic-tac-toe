492f0bfc4dd4ef5a9e211df90bfb885119fa2bf3
RachelIsrael <racheleisrael4@gmail.com>
2020-02-10 13:21:47 -0500

    fixed reset

--- challenge.js
+++ challenge.js
@@ -2,28 +2,31 @@ let square = document.querySelectorAll('.square');
 let button = document.querySelector('.clear');
 let background = 'red';
 let counter = document.querySelector('.turnCounter')
-counter.innerText = "Red's Turn"
+counter.innerText = "Red's Turn";
 counter.style.color = 'red';
 for(let i=0; i<square.length; i++){
-square[i].addEventListener('click', colorChange);
-function colorChange(e){
-    e.preventDefault();
-    square[i].style.backgroundColor = background;
-    if(background == 'red'){
-        background = 'blue';
-        counter.innerText = "Blue's Turn";
-        counter.style.color = 'blue';
-    }else{
+    square[i].addEventListener('click', colorChange);
+    function colorChange(e){
+        e.preventDefault();
+        square[i].style.backgroundColor = background;
+        if(background == 'red'){
+            background = 'blue';
+            counter.innerText = "Blue's Turn";
+            counter.style.color = 'blue';
+        }else{
+            background = 'red';
+            counter.innerText = "Red's Turn";
+            counter.style.color = 'red';
+        }
+        square[i].removeEventListener('click', colorChange)
+        }
+    button.addEventListener('click', clearBoard);
+    function clearBoard(e){
+        e.preventDefault();
+        square[i].style.backgroundColor = 'white';
         background = 'red';
         counter.innerText = "Red's Turn";
-        counter.style.color = 'red';
+        square[i].addEventListener('click', colorChange);
+        }
     }
-    square[i].removeEventListener('click', colorChange)
-    }
-button.addEventListener('click', clearBoard);
-function clearBoard(e){
-    e.preventDefault();
-    square[i].style.backgroundColor = 'white';
-    background = 'red';
-}
-}
+
