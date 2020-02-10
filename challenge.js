1a7cc7ff09897f827e4cdebc10c618f4fa1bc178
RachelIsrael <racheleisrael4@gmail.com>
2020-02-10 13:56:04 -0500

    array of win combinations

--- challenge.js
+++ challenge.js
@@ -4,6 +4,7 @@ let background = 'red';
 let counter = document.querySelector('.turnCounter')
 counter.innerText = "Red's Turn";
 counter.style.color = 'red';
+let win = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']]
 for(let i=0; i<square.length; i++){
     square[i].addEventListener('click', colorChange);
     function colorChange(e){
@@ -18,14 +19,20 @@ for(let i=0; i<square.length; i++){
             counter.innerText = "Red's Turn";
             counter.style.color = 'red';
         }
+        console.log(e.target.id)
         square[i].removeEventListener('click', colorChange)
+        // checkForWinner();
         }
+    // function checkForWinner(){
+
+    // }
     button.addEventListener('click', clearBoard);
     function clearBoard(e){
         e.preventDefault();
         square[i].style.backgroundColor = 'white';
         background = 'red';
         counter.innerText = "Red's Turn";
+        counter.style.color = 'red';
         square[i].addEventListener('click', colorChange);
         }
     }
